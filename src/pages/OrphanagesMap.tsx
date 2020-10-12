b31616833e1a9a175338519e5bdeb5b403880465
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-10-12 13:47:22 -0300

    Usando leaflet map para renderizar o mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React from "react";
+import { FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import mapMarkerImg from "../images/map-marker.svg";
+import { Map, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import "../styles/pages/orphanags-map.css";
 
-function OrphanagesMap(){
+function OrphanagesMap() {
 	return (
-		<h1>Hello World</h1>
+		<div id="page-map">
+			<aside>
+				<header>
+					<img src={mapMarkerImg} alt="Happy marker" />
+					<h2>Escolha um orfanato no mapa</h2>
+					<p>Muitas crianças estão esperando a sua visita :)</p>
+				</header>
+				<footer>
+					<strong>Cruzeiro</strong>
+					<span>São Paulo</span>
+				</footer>
+			</aside>
+			<Map
+				center={[-22.5725108,-44.9804947]}
+				zoom={15}
+				style={{ width: "100%", height: "100%" }}
+			>
+				<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				</Map>
+			<Link to="" className="create-orphanage">
+				<FiPlus size={32} color="#FFF" />
+			</Link>
+		</div>
 	);
 }
 
